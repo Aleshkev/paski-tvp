468d17716e3dff0f13832c707d1834be6da1d4e8
Jonasz Aleszkiewicz <j.aleszkiewicz@gmail.com>
2019-09-05 20:45:10 +0200

    544
    To trochę kompromitujące, ale dodałem przypadkiem zduplikowane paski, tylko z usuniętą interpunkcją.

--- cleanup.py
+++ cleanup.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import string
 
 p = Path("paski.txt")
 s = p.read_text("utf-8").upper()
@@ -6,7 +7,18 @@ s = p.read_text("utf-8").upper()
 for k, v in {'„': '"', '”': '"', "…": "...", "–": "-", "—": "-"}.items():
     s = s.replace(k, v)
 
-s = "\n".join(sorted(x for x in set(x.strip() for x in s.splitlines()) if x))
+t = set(x for x in set(x.strip() for x in s.splitlines()) if x)
+
+met = {}
+for i in t:
+    j = "".join(c for c in i if c not in string.punctuation + string.whitespace)
+    if j in met.keys():
+        print(f"Kolizja interpunkcyjna: {met[j]}, {i}")
+    if "  " in i:
+        print(f"Podwójna spacja: {i}")
+    met[j] = i
+
+s = "\n".join(sorted(t))
 
 print(f"Paski: {len(s.splitlines())}")
 
