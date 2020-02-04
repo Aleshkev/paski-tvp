b00c19744b29ec7f58d79a6ce10de19748da5a5c
Jonasz Aleszkiewicz <j.aleszkiewicz@gmail.com>
2020-02-04 17:06:20 +0100

    7495

--- cleanup.py
+++ cleanup.py
@@ -4,7 +4,7 @@ import string
 p = Path("paski.txt")
 s = p.read_text("utf-8").upper()
 
-for k, v in {'„': '"', '”': '"', "…": "...", "–": "-", "—": "-"}.items():
+for k, v in {'„': '"', '”': '"', "…": "...", "–": "-", "—": "-", "  ": " "}.items():
     s = s.replace(k, v)
 
 t = set(x for x in set(x.strip() for x in s.splitlines()) if x)
